264bdbcc18f9fece9b1a38df6efe6653a8fc9aeb
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-11-28 13:26:32 +0200

    Created add recipe component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,11 +7,12 @@ import { AboutUsComponent } from "./components/about-us/about-us.component";
 import { LoginComponent } from "./components/user/login/login.component";
 import { RegisterComponent } from './components/user/register/register.component';
 import { ProfileComponent } from './components/user/profile/profile.component';
+import { AddRecipeComponent } from './components/recipes/add-recipe/add-recipe.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AboutUsComponent, LoginComponent, RegisterComponent, ProfileComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AboutUsComponent, LoginComponent, RegisterComponent, ProfileComponent, AddRecipeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

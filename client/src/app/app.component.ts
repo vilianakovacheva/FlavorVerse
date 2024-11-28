bebdf48435868316f4976fc1dc2fc598351110dc
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-11-28 11:48:45 +0200

    Created about us component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/core/header/header.component";
 import { FooterComponent } from "./components/core/footer/footer.component";
 import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from "./components/about-us/about-us.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

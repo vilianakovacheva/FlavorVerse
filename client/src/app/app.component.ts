8708ef44e5f5b2c6d99ab301627d5f688276818a
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-11-28 12:00:05 +0200

    Created login component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./components/core/header/header.component";
 import { FooterComponent } from "./components/core/footer/footer.component";
 import { HomeComponent } from './components/home/home.component';
 import { AboutUsComponent } from "./components/about-us/about-us.component";
+import { LoginComponent } from "./components/user/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AboutUsComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AboutUsComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

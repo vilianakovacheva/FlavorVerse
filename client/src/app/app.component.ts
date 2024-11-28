b11601530be2cce05a6d507e7c7f85acedd3ed4d
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-11-28 11:44:36 +0200

    Created home component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/core/header/header.component";
 import { FooterComponent } from "./components/core/footer/footer.component";
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

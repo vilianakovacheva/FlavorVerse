cdf81eeb3ec626cfac5e39baf850a47f664d50ae
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-12-06 01:04:55 +0200

    Added the functionality to see recipe details and also created the delete functionality

--- client/src/app/components/recipes/recipe-details/recipe-details.component.ts
+++ client/src/app/components/recipes/recipe-details/recipe-details.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../../types/recipe';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ApiService } from '../../../api.service';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-recipe-details',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './recipe-details.component.html',
   styleUrl: './recipe-details.component.css'
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit {
+  recipe = {} as Recipe;
+  currentUserId: string = '';
+  isAuthor: boolean = false;
+  showModal:boolean = false;
+  recipeId: string = '';
+
+  constructor(private route: ActivatedRoute, private apiService: ApiService, private userService: UserService, private router: Router) {}
+
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+
+  openModal():void {
+    this.showModal = true;
+  }
+
+  cancelDelete():void {
+    this.showModal = false;
+  }
+
+  confirmDelete(): void {
+    this.apiService.deleteRecipe(this.recipeId).subscribe(
+      ()=> {
+        this.router.navigate(['/recipes']);
+      },
+      (error) => {
+        console.error('Error deleting recipe:', error);
+      }
+    )
+  }
+ 
+  ngOnInit(): void {
+    this.userService.getProfile().subscribe((data) => {
+      this.currentUserId = data._id;
+    })
+    const id = this.route.snapshot.params['recipeId'];
+    this.recipeId = id;
+
+    this.apiService.getRecipeDetails(id).subscribe((recipe) => {
+      this.recipe = recipe;
+      this.isAuthor = this.recipe._ownerId === this.currentUserId;
+    })
+  }
 
 }

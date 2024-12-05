f05e06b6153b439cc2d592756bcc24abf48c2e05
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-12-06 00:58:07 +0200

    Added functionality for logged-in users to create recipes and also integrated form validation

--- client/src/app/components/recipes/add-recipe/add-recipe.component.ts
+++ client/src/app/components/recipes/add-recipe/add-recipe.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../api.service';
+import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-add-recipe',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-recipe.component.html',
   styleUrl: './add-recipe.component.css'
 })
 export class AddRecipeComponent {
+  errorMessage: string | null = null;
 
+  constructor(private apiService: ApiService, private router: Router, private userService: UserService) {}
+
+  addRecipe(form: NgForm) {
+
+    if(form.invalid) {
+      return;
+    }
+
+    const { name, img, description, prepTime, cookTime, servings, calories, ingredients, steps } = form.value;
+
+    const ingredientsArray = ingredients.split('/').map((ing: string) => ing.trim());
+    const stepsArray = steps.split('/').map((step: string) => step.trim());
+
+    this.apiService.addRecipe(
+      name,
+      img,
+      description,
+      Number(prepTime),
+      Number(cookTime),
+      Number(servings),
+      Number(calories),
+      ingredientsArray,
+      stepsArray,
+    ).subscribe({
+      next: () => this.router.navigate(['/recipes']),
+      error: (err) => {
+        this.errorMessage = 'Failed to add recipe. Please try again!';
+        console.error(err);
+      }
+    });
+  }
 }

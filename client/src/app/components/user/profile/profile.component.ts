4bbea0f9c5c0c36811bacdd528f71a84992656d2
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-12-06 01:08:41 +0200

    Added the functionality for the profile page where the logged in user can see his/her email and the added recipes from he/her

--- client/src/app/components/user/profile/profile.component.ts
+++ client/src/app/components/user/profile/profile.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../../types/recipe';
+import { UserService } from '../user.service';
+import { ApiService } from '../../../api.service';
+import { RouterLink } from '@angular/router';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LoaderComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  recipes: Recipe[] = [];
+  email: string = '';
+  isLoading = true;
 
+  constructor(private userService: UserService, private apiService: ApiService) {}
+
+
+  ngOnInit(): void {
+    this.userService.getProfile().subscribe((data) => {
+      this.email = data.email;
+
+      this.apiService.getRecipesByUser(data._id).subscribe((recipes) => {
+        this.recipes = recipes;
+        this.isLoading = false;
+      });
+    })
+
+  }
 }
+

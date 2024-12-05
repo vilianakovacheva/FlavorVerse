54398b58f61f6796f915ac1d4243d04c2a767bcc
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-12-06 00:55:04 +0200

    All added recipes are now displayed in the recipes catalog

--- client/src/app/components/recipes/recipes-catalog/recipes-catalog.component.ts
+++ client/src/app/components/recipes/recipes-catalog/recipes-catalog.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../../types/recipe';
+import { LoaderComponent } from '../../shared/loader/loader.component';
+import { ApiService } from '../../../api.service';
+import { UserService } from '../../user/user.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-catalog',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent, RouterLink],
   templateUrl: './recipes-catalog.component.html',
   styleUrl: './recipes-catalog.component.css'
 })
-export class RecipesCatalogComponent {
+export class RecipesCatalogComponent implements OnInit {
+  recipes: Recipe[] = [];
+  isLoading = true;
+  
+  constructor(private apiService: ApiService, private userService: UserService) {}
+
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+
+  ngOnInit(): void {
+    this.apiService.getRecipes().subscribe((recipes) => {
+      this.recipes = recipes;
+      this.isLoading = false;
+    })
+  }
 
 }

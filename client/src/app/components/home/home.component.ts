f0e409a5485fcc104614621bc88bc006f501e1b0
vilianakovacheva <vilianakovacheva04@gmail.com>
2024-12-06 01:01:42 +0200

    Added the functionality on the home page to see the recent added recipes

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../user/user.service';
+import { Recipe } from '../../types/recipe';
+import { ApiService } from '../../api.service';
+import { LoaderComponent } from '../shared/loader/loader.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, LoaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  constructor(private userService: UserService, private router: Router) {}
+export class HomeComponent implements OnInit {
+  recipes: Recipe[] = [];
+  isLoading = true;
+
+  constructor(private userService: UserService, private router: Router, private apiService: ApiService) {}
 
   get isLoggedIn(): boolean {
     return this.userService.isLogged;
+  }
+
+  ngOnInit(): void {
+    this.apiService.getRecentRecipes().subscribe((recipes) => {
+      this.recipes = recipes.slice(0, 4);
+      this.isLoading = false;
+    })
   }
 }
